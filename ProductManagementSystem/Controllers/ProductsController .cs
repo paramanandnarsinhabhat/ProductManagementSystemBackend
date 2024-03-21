921fd2732a52004a68df602ef9fbf362621f88dd
Param <paramanandbhat3@gmail.com>
2024-03-21 12:49:39 +0530

    feat: Initialize ProductsController with ProductService DI

--- ProductManagementSystem/Controllers/ProductsController .cs
+++ ProductManagementSystem/Controllers/ProductsController .cs
@@ -1,11 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProductManagementSystem.Services;
 
 namespace ProductManagementSystem.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class ProductsController : Controller
     {
+        private readonly ProductService _productService;
 
-
-        
+        public ProductsController(ProductService productService)
+        {
+            _productService = productService;
+        }
     }
 }
