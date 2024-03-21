76f418c2818112ace335534c80846650b3c505c7
Param <paramanandbhat3@gmail.com>
2024-03-21 13:32:41 +0530

    feat: Implement POST endpoint for adding new products

--- ProductManagementSystem/Controllers/ProductsController .cs
+++ ProductManagementSystem/Controllers/ProductsController .cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProductManagementSystem.Models;
 using ProductManagementSystem.Services;
 
 namespace ProductManagementSystem.Controllers
@@ -12,6 +13,27 @@ namespace ProductManagementSystem.Controllers
         public ProductsController(ProductService productService)
         {
             _productService = productService;
+        }
+
+
+        // POST api/products
+        [HttpPost]
+        public async Task<ActionResult<Product>> PostProduct([FromBody] Product product)
+        {
+            try
+            {
+                var createdProduct = await _productService.AddProductAsync(product);
+
+                // Return an HTTP 201 Created response with the created product
+                return CreatedAtAction(nameof(PostProduct), new { id = createdProduct.Id }, createdProduct);
+            }
+            catch (Exception ex)
+            {
+             
+                // Return a generic HTTP 500 Internal Server Error response
+                // if something goes wrong beyond the client's control
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
     }
 }
