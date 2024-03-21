9a915c15cd4540316fb4dd072796ae05f28399ba
Param <paramanandbhat3@gmail.com>
2024-03-21 12:33:31 +0530

    feat: Implement AddProductAsync in ProductService

---
+++ ProductManagementSystem/Services/ProductService.cs
@@ -0,0 +1,21 @@
+﻿using ProductManagementSystem.Models;
+using ProductManagementSystem.Repositories;
+
+namespace ProductManagementSystem.Services
+{
+    public class ProductService
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<Product> AddProductAsync(Product product)
+        {
+            return await _productRepository.AddProductAsync(product);
+        }
+
+    }
+}
