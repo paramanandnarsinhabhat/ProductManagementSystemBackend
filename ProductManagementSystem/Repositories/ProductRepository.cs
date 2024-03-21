e87390c8d3d7797f13a20083d7b7a67d89e58ced
Param <paramanandbhat3@gmail.com>
2024-03-21 12:11:30 +0530

    feat: Add initial ProductRepository with AddProductAsync method stub

---
+++ ProductManagementSystem/Repositories/ProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using ProductManagementSystem.Data;
+using ProductManagementSystem.Models;
+
+namespace ProductManagementSystem.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly AppDbContext _context;
+        public ProductRepository(AppDbContext context) {
+            _context = context;
+        
+        }
+
+        public Task<Product> AddProductAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
